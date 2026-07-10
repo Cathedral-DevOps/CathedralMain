6b4b5c014ce6f12d4df62ffbe6ed9a4bd38a96d4
CathedralDevops <iesushominumsalvator14@gmail.com>
2026-07-10 16:13:52 -0500

    Add GSAP animations for grid containers and paragraph/image elements

--- static/js/index.js
+++ static/js/index.js
@@ -1,6 +1,8 @@
 document.addEventListener("DOMContentLoaded", () => {
   const items = document.querySelectorAll(".item");
-
+  const grid_container_2 = document.querySelectorAll(".grid-container-2");
+  const paragraph_and_image_one_single = document.querySelectorAll("item-type-4");
+  const paragraph_and_image_two_single = document.querySelectorAll("item-type-3");
   if (items.length === 0) {
     console.warn("GSAP: no .item elements found");
     return;
@@ -16,6 +18,45 @@ document.addEventListener("DOMContentLoaded", () => {
       duration: 0.8,
       ease: "power1.out",
       stagger: 0.15,
+      overwrite: "auto",
+    }
+  );
+   gsap.fromTo(
+    grid_container_2,
+    { opacity: 0, visibility: "hidden", y: 45 },
+    {
+      opacity: 1,
+      visibility: "visible",
+      y: 0,
+      duration: 0.8,
+      ease: "power1.out",
+      stagger: 1,
+      overwrite: "auto",
+    }
+  );
+  gsap.fromTo(
+    paragraph_and_image_one_single,
+    { opacity: 0, visibility: "hidden", y: 45 },
+    {
+      opacity: 1,
+      visibility: "visible",
+      y: 0,
+      duration: 0.8,
+      ease: "power1.out",
+      stagger: 1,
+      overwrite: "auto",
+    }
+  );
+  gsap.fromTo(
+    paragraph_and_image_two_single,
+    { opacity: 0, visibility: "hidden", y: 45 },
+    {
+      opacity: 1,
+      visibility: "visible",
+      y: 0,
+      duration: 0.8,
+      ease: "power1.out",
+      stagger: 1,
       overwrite: "auto",
     }
   );
